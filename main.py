c4d733bc544a8965a6a74ae9e8b5b75888d2c3c1
T. Hamilton <profhamilton3@gmail.com>
2024-08-29 16:04:33 -0700

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+
+#Libraries are added by extensions the build
+DFRobotMaqueenPlus.i2c_init()
+
+DFRobotMaqueenPlus.motot_run(Motors.M1, Dir.CW, 0)
---
+++ main.ts
@@ -0,0 +1,3 @@
+// Libraries are added by extensions the build
+DFRobotMaqueenPlus.I2CInit()
+DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 0)
