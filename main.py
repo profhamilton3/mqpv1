7dc1d280bd4e4037b0ebed13f33f83cc9206d3e7
T. Hamilton <profhamilton3@gmail.com>
2024-08-29 16:08:08 -0700

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -2,4 +2,8 @@
 #Libraries are added by extensions the build
 DFRobotMaqueenPlus.i2c_init()
 
-DFRobotMaqueenPlus.motot_run(Motors.M1, Dir.CW, 0)
+DFRobotMaqueenPlus.motot_run(Motors.ALL, Dir.CW, 30)
+DFRobotMaqueenPlus.motot_run(Motors.M2, Dir.CW, 30)
+pause(1300)
+basic.show_string("Hello!")
+DFRobotMaqueenPlus.motot_stop(Motors.ALL)
--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
 // Libraries are added by extensions the build
 DFRobotMaqueenPlus.I2CInit()
-DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 0)
+DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 30)
+DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 30)
+pause(1300)
+basic.showString("Hello!")
+DFRobotMaqueenPlus.mototStop(Motors.ALL)
