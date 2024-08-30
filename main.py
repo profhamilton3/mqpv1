56d19bfd176fb73205152defa598993e06e51dd9
T. Hamilton <profhamilton3@gmail.com>
2024-08-30 11:37:25 -0700

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 
 #Libraries are added by extensions the build
 
-def doDriveForward(speed:number, duration: number):
+def do_drive_forward(speed:number, duration: number):
     DFRobotMaqueenPlus.servo_run(Servos.S1, 15)
     DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBA, Color.GREEN)
     DFRobotMaqueenPlus.motot_run(Motors.ALL, Dir.CW, speed)
     pause(duration)
     DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBA, Color.OFF)
 
-def doDriveBackwar(speed:number, duration: number):
+def do_drive_backwar(speed:number, duration: number):
     music.tone_playable(Note.CSHARP, music.beat(BeatFraction.HALF))
     DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBA, Color.YELLOW)
     DFRobotMaqueenPlus.servo_run(Servos.S1, 25)
@@ -17,15 +17,15 @@ def doDriveBackwar(speed:number, duration: number):
     music.tone_playable(Note.CSHARP, music.beat(BeatFraction.HALF))
     DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBA, Color.OFF)
 
-def doDriveSpinRight(speed:number, duration: number):
+def do_drive_spin_right(speed:number, duration: number):
     DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBR, Color.CYAN)
     DFRobotMaqueenPlus.motot_run(Motors.M1, Dir.CCW, speed)
     DFRobotMaqueenPlus.motot_run(Motors.M2, Dir.CW, speed)
     pause(duration)
     DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBA, Color.OFF)
 
-def doDriveSpinLeft(speed:number, duration: number):
-        DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBR, Color.CYAN)
+def do_drive_spin_left(speed:number, duration: number):
+        DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBL, Color.CYAN)
         DFRobotMaqueenPlus.motot_run(Motors.M1, Dir.CW, speed)
         DFRobotMaqueenPlus.motot_run(Motors.M2, Dir.CCW, speed)
         pause(duration)
@@ -33,15 +33,43 @@ def doDriveSpinLeft(speed:number, duration: number):
 
 
 
-def on_button_pressed_a():
+def onIn_background():
     DFRobotMaqueenPlus.i2c_init()
+    basic.show_icon(IconNames.ANGRY)
+    while True:      
+        if 15 > DFRobotMaqueenPlus.ultra_sonic(PIN.P1, PIN.P2):
+            do_drive_backwar(100, 750)
+            DFRobotMaqueenPlus.motot_stop(Motors.ALL)
+            do_drive_spin_left(75,750)
+            DFRobotMaqueenPlus.motot_stop(Motors.ALL)
+        else:
+            basic.pause(200)
+
+control.in_background(onIn_background)
+
+def on_logo_event_pressed():
     DFRobotMaqueenPlus.servo_run(Servos.S1, 0)
-    doDriveForward(75,1000)
-    doDriveSpinLeft(50,500)
-    doDriveBackward(75,1000)
-    doDriveSpinRight(50, 500)
+    do_drive_forward(100,1500)
     DFRobotMaqueenPlus.motot_stop(Motors.ALL)
+    do_drive_spin_right(80,750)
+    DFRobotMaqueenPlus.motot_stop(Motors.ALL)
+    do_drive_backwar(100,1000)
+    DFRobotMaqueenPlus.motot_stop(Motors.ALL)
+    do_drive_spin_left(80, 750)
+    DFRobotMaqueenPlus.motot_stop(Motors.ALL)
+    do_drive_forward(35,1000)
+    DFRobotMaqueenPlus.motot_stop(Motors.ALL)
+    DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBA, Color.RED)
+
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
+
+def on_button_pressed_a():
+    music.built_in_playable_melody(Melodies.POWER_UP)
+    DFRobotMaqueenPlus.servo_run(Servos.S1, 60)
 
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+
+
 basic.show_icon(IconNames.ANGRY)
+music.set_volume(60)
--- main.ts
+++ main.ts
@@ -1,20 +1,23 @@
 // Libraries are added by extensions the build
-DFRobotMaqueenPlus.I2CInit()
-function doDriveForward(speed: number, duration: number) {
+function do_drive_forward(speed: number, duration: number) {
+    DFRobotMaqueenPlus.servoRun(Servos.S1, 15)
     DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.GREEN)
     DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, speed)
     pause(duration)
     DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.OFF)
 }
 
-function doDriveBackwar(speed: number, duration: number) {
+function do_drive_backwar(speed: number, duration: number) {
+    music.tonePlayable(Note.CSharp, music.beat(BeatFraction.Half))
     DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.YELLOW)
+    DFRobotMaqueenPlus.servoRun(Servos.S1, 25)
     DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, speed)
     pause(duration)
+    music.tonePlayable(Note.CSharp, music.beat(BeatFraction.Half))
     DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.OFF)
 }
 
-function doDriveSpinRight(speed: number, duration: number) {
+function do_drive_spin_right(speed: number, duration: number) {
     DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBR, Color.CYAN)
     DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, speed)
     DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, speed)
@@ -22,14 +25,46 @@ function doDriveSpinRight(speed: number, duration: number) {
     DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.OFF)
 }
 
-function doDriveSpinLeft(speed: number, duration: number) {
-    DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBR, Color.CYAN)
+function do_drive_spin_left(speed: number, duration: number) {
+    DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBL, Color.CYAN)
     DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, speed)
     DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, speed)
     pause(duration)
     DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.OFF)
 }
 
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+control.inBackground(function onIn_background() {
+    DFRobotMaqueenPlus.I2CInit()
+    basic.showIcon(IconNames.Angry)
+    while (true) {
+        if (15 > DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2)) {
+            do_drive_backwar(100, 750)
+            DFRobotMaqueenPlus.mototStop(Motors.ALL)
+            do_drive_spin_left(75, 750)
+            DFRobotMaqueenPlus.mototStop(Motors.ALL)
+        } else {
+            basic.pause(200)
+        }
+        
+    }
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
+    DFRobotMaqueenPlus.servoRun(Servos.S1, 0)
+    do_drive_forward(100, 1500)
+    DFRobotMaqueenPlus.mototStop(Motors.ALL)
+    do_drive_spin_right(80, 750)
+    DFRobotMaqueenPlus.mototStop(Motors.ALL)
+    do_drive_backwar(100, 1000)
+    DFRobotMaqueenPlus.mototStop(Motors.ALL)
+    do_drive_spin_left(80, 750)
+    DFRobotMaqueenPlus.mototStop(Motors.ALL)
+    do_drive_forward(35, 1000)
+    DFRobotMaqueenPlus.mototStop(Motors.ALL)
+    DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.RED)
+})
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    music.builtInPlayableMelody(Melodies.PowerUp)
+    DFRobotMaqueenPlus.servoRun(Servos.S1, 60)
+})
+basic.showIcon(IconNames.Angry)
+music.setVolume(60)
