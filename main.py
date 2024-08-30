5186313a523d3413531908f7e131d3d647cc54d4
T. Hamilton <profhamilton3@gmail.com>
2024-08-29 17:31:58 -0700

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,47 @@
 
 #Libraries are added by extensions the build
 DFRobotMaqueenPlus.i2c_init()
+basic.show_icon(IconNames.ANGRY)
+DFRobotMaqueenPlus.servo_run(Servos.S1, 0)
 
-DFRobotMaqueenPlus.motot_run(Motors.ALL, Dir.CW, 30)
-DFRobotMaqueenPlus.motot_run(Motors.M2, Dir.CW, 30)
-pause(1300)
-basic.show_string("Hello!")
-DFRobotMaqueenPlus.motot_stop(Motors.ALL)
+
+def doDriveForward(speed:number, duration: number):
+    DFRobotMaqueenPlus.servo_run(Servos.S1, 15)
+    DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBA, Color.GREEN)
+    DFRobotMaqueenPlus.motot_run(Motors.ALL, Dir.CW, speed)
+    pause(duration)
+    DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBA, Color.OFF)
+
+def doDriveBackwar(speed:number, duration: number):
+    music.tone_playable(Note.CSHARP, music.beat(BeatFraction.HALF))
+    DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBA, Color.YELLOW)
+    DFRobotMaqueenPlus.servo_run(Servos.S1, 25)
+    DFRobotMaqueenPlus.motot_run(Motors.ALL, Dir.CCW, speed)
+    pause(duration)
+    music.tone_playable(Note.CSHARP, music.beat(BeatFraction.HALF))
+    DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBA, Color.OFF)
+
+def doDriveSpinRight(speed:number, duration: number):
+    DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBR, Color.CYAN)
+    DFRobotMaqueenPlus.motot_run(Motors.M1, Dir.CCW, speed)
+    DFRobotMaqueenPlus.motot_run(Motors.M2, Dir.CW, speed)
+    pause(duration)
+    DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBA, Color.OFF)
+
+def doDriveSpinLeft(speed:number, duration: number):
+        DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBR, Color.CYAN)
+        DFRobotMaqueenPlus.motot_run(Motors.M1, Dir.CW, speed)
+        DFRobotMaqueenPlus.motot_run(Motors.M2, Dir.CCW, speed)
+        pause(duration)
+        DFRobotMaqueenPlus.set_rgb_light(RGBLight.RGBA, Color.OFF)
+
+
+
+def on_button_pressed_a():
+    doDriveForward(75,1000)
+    doDriveSpinLeft(50,500)
+    doDriveBackward(75,1000)
+    doDriveSpinRight(50, 500)
+    DFRobotMaqueenPlus.motot_stop(Motors.ALL)
+
+input.on_button_pressed(Button.A, on_button_pressed_a)
--- main.ts
+++ main.ts
@@ -1,7 +1,35 @@
 // Libraries are added by extensions the build
 DFRobotMaqueenPlus.I2CInit()
-DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 30)
-DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 30)
-pause(1300)
-basic.showString("Hello!")
-DFRobotMaqueenPlus.mototStop(Motors.ALL)
+function doDriveForward(speed: number, duration: number) {
+    DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.GREEN)
+    DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, speed)
+    pause(duration)
+    DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.OFF)
+}
+
+function doDriveBackwar(speed: number, duration: number) {
+    DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.YELLOW)
+    DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, speed)
+    pause(duration)
+    DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.OFF)
+}
+
+function doDriveSpinRight(speed: number, duration: number) {
+    DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBR, Color.CYAN)
+    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, speed)
+    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, speed)
+    pause(duration)
+    DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.OFF)
+}
+
+function doDriveSpinLeft(speed: number, duration: number) {
+    DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBR, Color.CYAN)
+    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, speed)
+    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, speed)
+    pause(duration)
+    DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.OFF)
+}
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+})
